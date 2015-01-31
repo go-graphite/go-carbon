804485b4a5e178a9e7a4b5cbf7d5d2a2d7c8db0e
lomik <r.lomonosov@gmail.com>
2015-01-31 15:19:06 +0300

    receiver simple tests

--- udp_receiver_test.go
+++ udp_receiver_test.go
@@ -4,8 +4,6 @@ import (
 	"net"
 	"testing"
 	"time"
-
-	"github.com/k0kubun/pp"
 )
 
 type udpTestCase struct {
@@ -58,8 +56,13 @@ func (test *udpTestCase) Send(text string) {
 	}
 }
 
-func TestUdpReceiver1(t *testing.T) {
+func (test *udpTestCase) Eq(a *Message, b *Message) {
+	if a.Name != b.Name || a.Value != b.Value || a.Timestamp != b.Timestamp {
+		test.Fatalf("%#v != %#v", a, b)
+	}
+}
 
+func TestUdpReceiver1(t *testing.T) {
 	test := newUdpTestCase(t)
 	defer test.Finish()
 
@@ -67,13 +70,45 @@ func TestUdpReceiver1(t *testing.T) {
 
 	time.Sleep(10 * time.Millisecond)
 
-	for i := 0; i < 1; i++ {
-		select {
-		case msg := <-test.rcvChan:
-			pp.Println(msg)
-		default:
-			t.Fatalf("Message #%d not received", i)
-		}
+	select {
+	case msg := <-test.rcvChan:
+		test.Eq(msg, &Message{
+			Name:      "hello.world",
+			Value:     42.15,
+			Timestamp: 1422698155,
+		})
+	default:
+		t.Fatalf("Message #0 not received")
+	}
+}
+
+func TestUdpReceiver2(t *testing.T) {
+	test := newUdpTestCase(t)
+	defer test.Finish()
+
+	test.Send("hello.world 42.15 1422698155\nmetric.name -72.11 1422698155\n")
+
+	time.Sleep(10 * time.Millisecond)
+
+	select {
+	case msg := <-test.rcvChan:
+		test.Eq(msg, &Message{
+			Name:      "hello.world",
+			Value:     42.15,
+			Timestamp: 1422698155,
+		})
+	default:
+		t.Fatalf("Message #0 not received")
 	}
 
+	select {
+	case msg := <-test.rcvChan:
+		test.Eq(msg, &Message{
+			Name:      "metric.name",
+			Value:     -72.11,
+			Timestamp: 1422698155,
+		})
+	default:
+		t.Fatalf("Message #1 not received")
+	}
 }
