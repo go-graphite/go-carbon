375cc0c00f7ddf5770773ebf36c26bf968365d11
lomik <r.lomonosov@gmail.com>
2015-02-23 14:14:03 +0300

    Purge incomplete lines storage (from udp messages greather MTU) no more than once per second

--- receiver/udp.go
+++ receiver/udp.go
@@ -34,29 +34,31 @@ func NewUDP(out chan *points.Points) *UDP {
 }
 
 type incompleteRecord struct {
-	added time.Time
-	data  []byte
+	deadline time.Time
+	data     []byte
 }
 
 // incompleteStorage store incomplete lines
 type incompleteStorage struct {
-	Records map[string]*incompleteRecord
-	Expires time.Duration
-	MaxSize int
+	Records   map[string]*incompleteRecord
+	Expires   time.Duration
+	NextPurge time.Time
+	MaxSize   int
 }
 
 func newIncompleteStorage() *incompleteStorage {
 	return &incompleteStorage{
-		Records: make(map[string]*incompleteRecord, 0),
-		Expires: 5 * time.Second,
-		MaxSize: 10000,
+		Records:   make(map[string]*incompleteRecord, 0),
+		Expires:   5 * time.Second,
+		MaxSize:   10000,
+		NextPurge: time.Now().Add(time.Second),
 	}
 }
 
 func (storage *incompleteStorage) store(addr string, data []byte) {
 	storage.Records[addr] = &incompleteRecord{
-		added: time.Now(),
-		data:  data,
+		deadline: time.Now().Add(storage.Expires),
+		data:     data,
 	}
 	storage.checkAndClear()
 }
@@ -64,7 +66,7 @@ func (storage *incompleteStorage) store(addr string, data []byte) {
 func (storage *incompleteStorage) pop(addr string) []byte {
 	if record, ok := storage.Records[addr]; ok {
 		delete(storage.Records, addr)
-		if record.added.Before(time.Now().Add(-storage.Expires)) {
+		if record.deadline.Before(time.Now()) {
 			return nil
 		}
 		return record.data
@@ -73,18 +75,23 @@ func (storage *incompleteStorage) pop(addr string) []byte {
 }
 
 func (storage *incompleteStorage) purge() {
-	die := time.Now().Add(-storage.Expires)
+	now := time.Now()
 	for key, record := range storage.Records {
-		if record.added.Before(die) {
+		if record.deadline.Before(now) {
 			delete(storage.Records, key)
 		}
 	}
+	storage.NextPurge = time.Now().Add(time.Second)
 }
 
 func (storage *incompleteStorage) checkAndClear() {
-	if len(storage.Records) > storage.MaxSize {
-		storage.purge()
+	if len(storage.Records) < storage.MaxSize {
+		return
 	}
+	if storage.NextPurge.After(time.Now()) {
+		return
+	}
+	storage.purge()
 }
 
 // Addr returns binded socket address. For bind port 0 in tests
