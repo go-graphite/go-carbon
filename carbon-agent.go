38a7cacde46911454a2e11b3c4b9c0f371f86262
lomik <r.lomonosov@gmail.com>
2016-01-05 19:05:21 +0300

    version 0.7-beta3

--- carbon-agent.go
+++ carbon-agent.go
@@ -22,7 +22,7 @@ import (
 import _ "net/http/pprof"
 
 // Version of go-carbon
-const Version = "0.7-beta2"
+const Version = "0.7-beta3"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
