84d32612bb7b159398536cbf8ce7a91c30607f58
lomik <r.lomonosov@gmail.com>
2016-06-17 22:49:36 +0300

    version bump. v0.7.2

--- carbon-agent.go
+++ carbon-agent.go
@@ -22,7 +22,7 @@ import (
 import _ "net/http/pprof"
 
 // Version of go-carbon
-const Version = "0.7.1"
+const Version = "0.7.2"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
