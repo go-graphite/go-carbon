f2824707d9306b76795dc4dc858b6624ef3ea8ff
Roman Lomonosov <r.lomonosov@gmail.com>
2017-08-01 23:54:33 +0300

    change bad chmod from 0.10 release #67

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 systemctl daemon-reload 2> /dev/null || true
+
+# fix broken chmod in release 0.10 packed by travis-ci
+chmod 644 /etc/logrotate.d/go-carbon || true
+chmod 644 /lib/systemd/system/go-carbon.service || true
+chmod 755 /etc/init.d/go-carbon || true
+chmod 644 /etc/go-carbon/go-carbon.conf || true
+chmod 644 /etc/go-carbon/storage-schemas.conf || true
+chmod 644 /etc/go-carbon/storage-aggregation.conf || true
