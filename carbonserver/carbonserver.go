go-graphite/go-carbon

commit c8c4b392e2e11a72cacbe83f14337fab2704aebd

Add context cancellation for find requests, and find part of render.

carbonserver/carbonserver.go
@@ -103,6 +103,14 @@ type QueryItem struct {
 	QueryFinished chan struct{}
 }
 
+//type GlobResponse struct {
+type ExpandedGlobResponse struct {
+	Name  string
+	Files []string
+	Leafs []bool
+	Err   error
+}
+
 var TraceHeaders = map[string]string{
 	"X-CTX-CarbonAPI-UUID":    "carbonapi_uuid",
 	"X-CTX-CarbonZipper-UUID": "carbonzipper_uuid",
@@ -675,7 +683,7 @@ func (listener *CarbonserverListener) updateFileList(dir string) {
 	)
 }
 
-func (listener *CarbonserverListener) expandGlobs(query string) ([]string, []bool, error) {
+func (listener *CarbonserverListener) expandGlobs(query string, resultCh chan<- *ExpandedGlobResponse, done chan<- struct{}) {
 	var useGlob bool
 	logger := zapwriter.Logger("carbonserver")
 
@@ -725,7 +733,7 @@ func (listener *CarbonserverListener) expandGlobs(query string) ([]string, []boo
 				for _, sub := range parts {
 					if len(newglobs) > listener.maxGlobs {
 						if listener.failOnMaxGlobs {
-							return nil, nil, errMaxGlobsExhausted
+							resultCh <- &ExpandedGlobResponse{query, nil, nil, errMaxGlobsExhausted}
 						}
 						break
 					}
@@ -734,7 +742,7 @@ func (listener *CarbonserverListener) expandGlobs(query string) ([]string, []boo
 			} else {
 				if len(newglobs) > listener.maxGlobs {
 					if listener.failOnMaxGlobs {
-						return nil, nil, errMaxGlobsExhausted
+						resultCh <- &ExpandedGlobResponse{query, nil, nil, errMaxGlobsExhausted}
 					}
 					break
 				}
@@ -818,7 +826,7 @@ func (listener *CarbonserverListener) expandGlobs(query string) ([]string, []boo
 		files[i] = strings.Replace(p, "/", ".", -1)
 	}
 
-	return files, leafs, nil
+	resultCh <- &ExpandedGlobResponse{query, files, leafs, nil}
 }
 
 func (listener *CarbonserverListener) Stat(send helper.StatCallback) {

carbonserver/find.go
@@ -2,6 +2,7 @@ package carbonserver
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -118,6 +119,9 @@ func (listener *CarbonserverListener) findHandler(wr http.ResponseWriter, req *h
 
 	var err error
 	fromCache := false
+	//rch := make(chan *findResponse)
+	//response := make(chan findResponse)
+	//var resp findResponse
 	if listener.findCacheEnabled {
 		key := strings.Join(query, ",") + "&" + format
 		size := uint64(100 * 1024 * 1024)
@@ -127,7 +131,7 @@ func (listener *CarbonserverListener) findHandler(wr http.ResponseWriter, req *h
 		if !ok {
 			logger.Debug("find cache miss")
 			atomic.AddUint64(&listener.metrics.FindCacheMiss, 1)
-			response, err = listener.findMetrics(logger, t0, formatCode, query)
+			response, err = listener.findMetrics(ctx, logger, t0, formatCode, query)
 			if err != nil {
 				item.StoreAbort()
 			} else {
@@ -140,7 +144,7 @@ func (listener *CarbonserverListener) findHandler(wr http.ResponseWriter, req *h
 			fromCache = true
 		}
 	} else {
-		response, err = listener.findMetrics(logger, t0, formatCode, query)
+		response, err = listener.findMetrics(ctx, logger, t0, formatCode, query)
 	}
 
 	if err != nil || response == nil {
@@ -200,53 +204,14 @@ type globs struct {
 	Leafs []bool
 }
 
-func (listener *CarbonserverListener) findMetrics(logger *zap.Logger, t0 time.Time, format responseFormat, names []string) (*findResponse, error) {
+func (listener *CarbonserverListener) findMetrics(ctx context.Context, logger *zap.Logger, t0 time.Time, format responseFormat, names []string) (*findResponse, error) {
 	var result findResponse
-	var expandedGlobs []globs
-	var errors []findError
-	var err error
 	metricsCount := uint64(0)
-	for _, name := range names {
-
-		glob := globs{
-			Name: name,
-		}
-		glob.Files, glob.Leafs, err = listener.expandGlobs(name)
-		if err != nil {
-			errors = append(errors, findError{name: name, err: err})
-			continue
-		}
-
-		expandedGlobs = append(expandedGlobs, glob)
+	expandedGlobs, err := listener.getExpandedGlobs(ctx, logger, t0, names)
+	if expandedGlobs == nil {
+		return nil, err
 	}
-
-	if len(errors) > 0 {
-		atomic.AddUint64(&listener.metrics.FindErrors, uint64(len(errors)))
-
-		if len(errors) == len(names) {
-			logger.Error("find failed",
-				zap.Duration("runtime_seconds", time.Since(t0)),
-				zap.String("reason", "can't expand globs"),
-				zap.Any("errors", errors),
-			)
-			return nil, fmt.Errorf("find failed, can't expand globs")
-		} else {
-			logger.Warn("find partly failed",
-				zap.Duration("runtime_seconds", time.Since(t0)),
-				zap.String("reason", "can't expand globs for some metrics"),
-				zap.Any("errors", errors),
-			)
-		}
-	}
-
 	atomic.AddUint64(&listener.metrics.MetricsFound, metricsCount)
-	logger.Debug("expandGlobs result",
-		zap.String("action", "expandGlobs"),
-		zap.Strings("metrics", names),
-		zap.String("format", format.String()),
-		zap.Any("result", expandedGlobs),
-	)
-
 	switch format {
 	case protoV3Format, jsonFormat:
 		var err error
@@ -356,4 +321,66 @@ func (listener *CarbonserverListener) findMetrics(logger *zap.Logger, t0 time.Ti
 		return &findResponse{buf.Bytes(), httpHeaders.ContentTypePickle, files}, nil
 	}
 	return nil, nil
+}
+
+func (listener *CarbonserverListener) getExpandedGlobs(ctx context.Context, logger *zap.Logger, t0 time.Time, names []string) ([]globs, error) {
+	var expandedGlobs []globs
+	var errors []findError
+	var err error
+	expGlobResultChan := make(chan *ExpandedGlobResponse, len(names))
+	done := make(chan struct{})
+
+	for _, name := range names {
+		go listener.expandGlobs(name, expGlobResultChan, done)
+	}
+	responseCount := 0
+GATHER:
+	for {
+		select {
+		case expandedResult := <-expGlobResultChan:
+			responseCount++
+			glob := globs{
+				Name: expandedResult.Name,
+			}
+			glob.Files, glob.Leafs, err = expandedResult.Files, expandedResult.Leafs, expandedResult.Err
+			if err != nil {
+				errors = append(errors, findError{name: expandedResult.Name, err: err})
+			}
+			expandedGlobs = append(expandedGlobs, glob)
+			if responseCount == len(names) {
+				break GATHER
+			}
+
+		case <-ctx.Done():
+			done <- struct{}{}
+			return nil, fmt.Errorf("find failed, timeout")
+		}
+	}
+
+	if len(errors) > 0 {
+		atomic.AddUint64(&listener.metrics.FindErrors, uint64(len(errors)))
+
+		if len(errors) == len(names) {
+			logger.Error("find failed",
+				zap.Duration("runtime_seconds", time.Since(t0)),
+				zap.String("reason", "can't expand globs"),
+				zap.Any("errors", errors),
+			)
+			return nil, fmt.Errorf("find failed, can't expand globs")
+		} else {
+			logger.Warn("find partly failed",
+				zap.Duration("runtime_seconds", time.Since(t0)),
+				zap.String("reason", "can't expand globs for some metrics"),
+				zap.Any("errors", errors),
+			)
+		}
+	}
+
+	logger.Debug("expandGlobs result",
+		zap.String("action", "expandGlobs"),
+		zap.Strings("metrics", names),
+		//zap.String("format", format.String()),
+		zap.Any("result", expandedGlobs),
+	)
+	return expandedGlobs, nil
 }

carbonserver/render.go
@@ -2,6 +2,7 @@ package carbonserver
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -203,7 +204,7 @@ func (listener *CarbonserverListener) renderHandler(wr http.ResponseWriter, req 
 		}
 	}()
 
-	response, fromCache, err := listener.fetchWithCache(logger, format, targets)
+	response, fromCache, err := listener.fetchWithCache(ctx, logger, format, targets)
 
 	wr.Header().Set("Content-Type", response.contentType)
 	if err != nil {
@@ -249,7 +250,7 @@ func (listener *CarbonserverListener) renderHandler(wr http.ResponseWriter, req 
 
 }
 
-func (listener *CarbonserverListener) fetchWithCache(logger *zap.Logger, format responseFormat, targets map[timeRange][]target) (fetchResponse, bool, error) {
+func (listener *CarbonserverListener) fetchWithCache(ctx context.Context, logger *zap.Logger, format responseFormat, targets map[timeRange][]target) (fetchResponse, bool, error) {
 	logger = logger.With(
 		zap.String("function", "fetchWithCache"),
 	)
@@ -297,7 +298,7 @@ func (listener *CarbonserverListener) fetchWithCache(logger *zap.Logger, format 
 			logger.Debug("query cache miss")
 			atomic.AddUint64(&listener.metrics.QueryCacheMiss, 1)
 
-			response, err = listener.prepareDataProto(format, targets)
+			response, err = listener.prepareDataProto(ctx, logger, format, targets)
 			if err != nil {
 				item.StoreAbort()
 			} else {
@@ -311,21 +312,42 @@ func (listener *CarbonserverListener) fetchWithCache(logger *zap.Logger, format 
 			fromCache = true
 		}
 	} else {
-		response, err = listener.prepareDataProto(format, targets)
+		response, err = listener.prepareDataProto(ctx, logger, format, targets)
 	}
 	return response, fromCache, err
 }
 
-func (listener *CarbonserverListener) prepareDataProto(format responseFormat, targets map[timeRange][]target) (fetchResponse, error) {
+func (listener *CarbonserverListener) prepareDataProto(ctx context.Context, logger *zap.Logger, format responseFormat, targets map[timeRange][]target) (fetchResponse, error) {
 	contentType := "application/text"
 	var b []byte
-	var err error
 	var metricsFetched int
 	var memoryUsed int
 	var valuesFetched int
 
 	var multiv3 protov3.MultiFetchResponse
 	var multiv2 protov2.MultiFetchResponse
+
+	metricMap := make(map[string]bool)
+
+	for _, ts := range targets {
+		for _, metric := range ts {
+			metricMap[metric.Name] = true
+		}
+	}
+	metricNames := make([]string, len(metricMap))
+	for k := range metricMap {
+		metricNames = append(metricNames, k)
+	}
+	expandedGlobs, err := listener.getExpandedGlobs(ctx, logger, time.Now(), metricNames)
+
+	if expandedGlobs == nil {
+		return fetchResponse{nil, contentType, 0, 0, 0, nil}, err
+	}
+
+	metricGlobMap := make(map[string]globs)
+	for _, expandedGlob := range expandedGlobs {
+		metricGlobMap[expandedGlob.Name] = expandedGlob
+	}
 
 	var metrics []string
 	for tr, ts := range targets {
@@ -334,53 +356,56 @@ func (listener *CarbonserverListener) prepareDataProto(format responseFormat, ta
 			untilTime := tr.until
 
 			listener.logger.Debug("fetching data...")
-			files, leafs, err := listener.expandGlobs(metric.Name)
-			if err != nil {
+
+			if expandedResult, ok := metricGlobMap[metric.Name]; ok {
+				files, leafs := expandedResult.Files, expandedResult.Leafs
+
+				metricsCount := 0
+				for i := range files {
+					if leafs[i] {
+						metricsCount++
+					}
+				}
+				listener.logger.Debug("expandGlobs result",
+					zap.String("handler", "render"),
+					zap.String("action", "expandGlobs"),
+					zap.String("metric", metric.Name),
+					zap.Int("metrics_count", metricsCount),
+					zap.Int32("from", fromTime),
+					zap.Int32("until", untilTime),
+				)
+
+				if format == protoV2Format || format == jsonFormat {
+					res, err := listener.fetchDataPB(metric.Name, files, leafs, fromTime, untilTime)
+					if err != nil {
+						atomic.AddUint64(&listener.metrics.RenderErrors, 1)
+						listener.logger.Error("error while fetching the data",
+							zap.Error(err),
+						)
+						continue
+					}
+					multiv2.Metrics = append(multiv2.Metrics, res.Metrics...)
+				} else {
+					res, err := listener.fetchDataPB3(metric.Name, files, leafs, fromTime, untilTime)
+					if err != nil {
+						atomic.AddUint64(&listener.metrics.RenderErrors, 1)
+						listener.logger.Error("error while fetching the data",
+							zap.Error(err),
+						)
+						continue
+					}
+					for i := range res.Metrics {
+						res.Metrics[i].PathExpression = metric.PathExpression
+					}
+					multiv3.Metrics = append(multiv3.Metrics, res.Metrics...)
+				}
+			} else {
 				listener.logger.Debug("expand globs returned an error",
 					zap.Error(err),
 				)
 				continue
 			}
 
-			metricsCount := 0
-			for i := range files {
-				if leafs[i] {
-					metricsCount++
-				}
-			}
-			listener.logger.Debug("expandGlobs result",
-				zap.String("handler", "render"),
-				zap.String("action", "expandGlobs"),
-				zap.String("metric", metric.Name),
-				zap.Int("metrics_count", metricsCount),
-				zap.Int32("from", fromTime),
-				zap.Int32("until", untilTime),
-			)
-
-			if format == protoV2Format || format == jsonFormat {
-				res, err := listener.fetchDataPB(metric.Name, files, leafs, fromTime, untilTime)
-				if err != nil {
-					atomic.AddUint64(&listener.metrics.RenderErrors, 1)
-					listener.logger.Error("error while fetching the data",
-						zap.Error(err),
-					)
-					continue
-				}
-				multiv2.Metrics = append(multiv2.Metrics, res.Metrics...)
-			} else {
-				res, err := listener.fetchDataPB3(metric.Name, files, leafs, fromTime, untilTime)
-				if err != nil {
-					atomic.AddUint64(&listener.metrics.RenderErrors, 1)
-					listener.logger.Error("error while fetching the data",
-						zap.Error(err),
-					)
-					continue
-				}
-				for i := range res.Metrics {
-					res.Metrics[i].PathExpression = metric.PathExpression
-				}
-				multiv3.Metrics = append(multiv3.Metrics, res.Metrics...)
-			}
 		}
 	}
 

commit 2ea3044d1950b8d7e191c5589b8be608217e7fe5

trie: fix a panics caused by bad query of missing ] and add a recover in expandGlobs
* should consider making a fuzzer test soon!
* panic crash in expandGlobs seems to be introduced by commit c8c4b392e

carbonserver/carbonserver.go
@@ -28,6 +28,7 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
+	"runtime/debug"
 	"sort"
 	"strconv"
 	"strings"
@@ -749,6 +750,12 @@ func (listener *CarbonserverListener) updateFileList(dir string) {
 }
 
 func (listener *CarbonserverListener) expandGlobs(ctx context.Context, query string, resultCh chan<- *ExpandedGlobResponse) {
+	defer func() {
+		if err := recover(); err != nil {
+			resultCh <- &ExpandedGlobResponse{query, nil, nil, fmt.Errorf("%s\n%s", err, debug.Stack())}
+		}
+	}()
+
 	logger := TraceContextToZap(ctx, listener.logger)
 	matchedCount := 0
 	defer func(start time.Time) {

carbonserver/trie.go
@@ -176,7 +176,7 @@ func newGlobState(expr string, expand func(globs []string) ([]string, error)) (*
 				s.c[expr[i]] = true
 				i++
 			}
-			if expr[i] != ']' {
+			if i >= len(expr) || expr[i] != ']' {
 				return nil, errors.New("glob: missing ]")
 			}
 

carbonserver/trie_test.go
@@ -118,9 +118,10 @@ func TestTrieIndex(t *testing.T) {
 		"/service-01/server-170/metric-namespace-006-xdp/cpu.wsp",
 	}
 	var cases = []struct {
-		input  []string
-		query  string
-		expect []string
+		input    []string
+		query    string
+		hasError bool
+		expect   []string
 	}{
 		{
 			input:  commonFiles,
@@ -400,6 +401,12 @@ func TestTrieIndex(t *testing.T) {
 			},
 		},
 		{
+			// reason: should no panic over bad queries
+			input:    commonFiles,
+			query:    "service-01.server-170.metric-namespace-004-007-xdp.cp[u",
+			hasError: true,
+		},
+		{
 			input: []string{
 				"/services/groups/xyz/xxx_404/nginx/type/prod/frontend/random-404_xoxo/http_3xx.wsp",
 				"/services/groups/xyz/xxx_404/nginx/type/prod/frontend/random-404_xoxo/http_5xx.wsp",
@@ -601,16 +608,16 @@ func TestTrieIndex(t *testing.T) {
 			t.Logf("case: TestTrieIndex/'^%s$'", regexp.QuoteMeta(c.query))
 
 			trieServer := newTrieServer(c.input, true)
-			trieFiles, _, err := trieServer.expandGlobsTrie(c.query)
-			if err != nil {
+			resultCh := make(chan *ExpandedGlobResponse, 1)
+			trieServer.expandGlobs(context.TODO(), c.query, resultCh)
+			result := <-resultCh
+			trieFiles, err := result.Files, result.Err
+			// trieFiles, _, err := trieServer.expandGlobsTrie(c.query)
+			if err != nil && !c.hasError {
 				t.Errorf("failed to trie.expandGlobs: %s", err)
 			}
 
 			sort.Strings(trieFiles)
-
-			if len(trieFiles) == 0 {
-				t.Errorf("trie.expandGlobs fetched 0 files")
-			}
 			if !reflect.DeepEqual(trieFiles, c.expect) {
 				t.Errorf("incorrect files retrieved\nreturns: %s\nexpect:  %s\n", trieFiles, c.expect)
 			}