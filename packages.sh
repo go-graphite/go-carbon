0df8b196c06e733a09f0d2820f748b4155f25023
Andrei Vereha <andrei.vereha@booking.com>
2020-07-13 12:32:37 +0200

    fix packages.sh
    It was failing with this error:
    ```
    Err:4 http://archive.ubuntu.com/ubuntu cosmic Release
      404  Not Found [IP: 91.189.88.142 80]
    ```
    
    This is because Ubuntu 18.10 is EOL.
    Updated to use 20.04 that is latest LTS: https://wiki.ubuntu.com/Releases
    
    Also updated go version to 1.14

--- packages.sh
+++ packages.sh
@@ -3,16 +3,16 @@
 cd `dirname $0`
 ROOT=$PWD
 
-docker run -ti --rm -v $ROOT:/root/go/src/github.com/go-graphite/go-carbon ubuntu:18.10 bash -c '
+docker run -ti --rm -v $ROOT:/root/go/src/github.com/go-graphite/go-carbon ubuntu:20.04 bash -c '
     cd /root/
-    export GO_VERSION=1.10.3
+    export GO_VERSION=1.14
     apt-get update
     apt-get install -y rpm ruby ruby-dev wget make git gcc
 
     wget https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
     tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
     ln -s /usr/local/go/bin/go /usr/local/bin/go
-    
+
     # newer fpm is broken https://github.com/jordansissel/fpm/issues/1612
     gem install rake
     gem install fpm:1.10.2
