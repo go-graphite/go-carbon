go-graphite/go-carbon

commit 52fe9e23f25a5f72aa34e16d60e0aacf401a15ee

Adding prometheus metrics for tracking cancelled and timed out requests.

carbonserver/carbonserver.go
@@ -289,6 +289,10 @@ type prometheus struct {
 
 	diskRequests      prom.Counter
 	diskRequest       func()
+	cancelledRequests prom.Counter
+	cancelledRequest  func()
+	timeoutRequests   prom.Counter
+	timeoutRequest    func()
 	diskWaitDurations prom.Histogram
 	diskWaitDuration  func(time.Duration)
 
@@ -338,6 +342,14 @@ func (c *CarbonserverListener) InitPrometheus(reg prom.Registerer) {
 		diskRequests: prom.NewCounter(prom.CounterOpts{
 			Name: "disk_requests_total",
 			Help: "Number of times disk has been hit",
+		}),
+		cancelledRequests: prom.NewCounter(prom.CounterOpts{
+			Name: "cancelled_requests_total",
+			Help: "Number of times a request has been cancelled",
+		}),
+		timeoutRequests: prom.NewCounter(prom.CounterOpts{
+			Name: "timeout_requests_total",
+			Help: "Number of times a request has been timeout",
 		}),
 		diskWaitDurations: prom.NewHistogram(
 			prom.HistogramOpts{
@@ -391,6 +403,8 @@ func (c *CarbonserverListener) InitPrometheus(reg prom.Registerer) {
 
 	reg.MustRegister(c.prometheus.requests)
 	reg.MustRegister(c.prometheus.cacheRequests)
+	reg.MustRegister(c.prometheus.cancelledRequests)
+	reg.MustRegister(c.prometheus.timeoutRequests)
 	reg.MustRegister(c.prometheus.durations)
 	reg.MustRegister(c.prometheus.diskRequests)
 	reg.MustRegister(c.prometheus.diskWaitDurations)
@@ -436,6 +450,8 @@ func NewCarbonserverListener(cacheGetFunc func(key string) []points.Point) *Carb
 			cacheRequest:     func(string, bool) {},
 			cacheDuration:    func(string, time.Duration) {},
 			diskRequest:      func() {},
+			cancelledRequest: func() {},
+			timeoutRequest:   func() {},
 			diskWaitDuration: func(time.Duration) {},
 			returnedMetric:   func() {},
 			returnedPoint:    func(int) {},

carbonserver/find.go
@@ -119,9 +119,6 @@ func (listener *CarbonserverListener) findHandler(wr http.ResponseWriter, req *h
 
 	var err error
 	fromCache := false
-	//rch := make(chan *findResponse)
-	//response := make(chan findResponse)
-	//var resp findResponse
 	if listener.findCacheEnabled {
 		key := strings.Join(query, ",") + "&" + format
 		size := uint64(100 * 1024 * 1024)
@@ -351,7 +348,13 @@ GATHER:
 			}
 
 		case <-ctx.Done():
-			return nil, fmt.Errorf("find failed, timeout")
+			switch ctx.Err() {
+			case context.DeadlineExceeded:
+				listener.prometheus.timeoutRequests.Inc()
+			case context.Canceled:
+				listener.prometheus.cancelledRequests.Inc()
+			}
+			return nil, fmt.Errorf("could not expand globs - %s", ctx.Err().Error())
 		}
 	}
 

commit 70e492324eb4eafa2db0aa2a3db1f432e0e5cdc5

carbonserver: fix two panics related to prometheus #374
It was introduced in commit 52fe9e23. Only triggered if prometheus
is not enabled.

carbonserver/carbonserver.go
@@ -358,6 +358,14 @@ func (c *CarbonserverListener) InitPrometheus(reg prom.Registerer) {
 		c.prometheus.diskRequests.Inc()
 	}
 
+	c.prometheus.cancelledRequest = func() {
+		c.prometheus.cancelledRequests.Inc()
+	}
+
+	c.prometheus.timeoutRequest = func() {
+		c.prometheus.timeoutRequests.Inc()
+	}
+
 	c.prometheus.diskWaitDuration = func(t time.Duration) {
 		c.prometheus.diskWaitDurations.Observe(t.Seconds())
 	}

carbonserver/find.go
@@ -362,9 +362,9 @@ GATHER:
 		case <-ctx.Done():
 			switch ctx.Err() {
 			case context.DeadlineExceeded:
-				listener.prometheus.timeoutRequests.Inc()
+				listener.prometheus.timeoutRequest()
 			case context.Canceled:
-				listener.prometheus.cancelledRequests.Inc()
+				listener.prometheus.cancelledRequest()
 			}
 			return nil, fmt.Errorf("could not expand globs - %s", ctx.Err().Error())
 		}