4ac29db95256242ec1ecd2553ac43bcc3cef61f8
Denys Zhdanov <denys.zhdanov@booking.com>
2024-05-21 13:31:50 +0200

    Migrate Prometheus instrumentation to collectors

--- go-carbon.go
+++ go-carbon.go
@@ -11,12 +11,13 @@ import (
 	"os"
 	"os/signal"
 	"os/user"
+	"regexp"
 	"runtime"
 	"strconv"
 	"syscall"
 
 	"github.com/lomik/zapwriter"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	daemon "github.com/sevlyar/go-daemon"
 
@@ -201,8 +202,12 @@ func main() {
 	}
 
 	if cfg.Prometheus.Enabled {
-		app.PromRegisterer.MustRegister(prometheus.NewGoCollector())
-		app.PromRegisterer.MustRegister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}))
+		app.PromRegisterer.MustRegister(
+			collectors.NewBuildInfoCollector(),
+			collectors.NewGoCollector(
+				collectors.WithGoCollectorRuntimeMetrics(collectors.GoRuntimeMetricsRule{Matcher: regexp.MustCompile("/.*")}),
+			),
+			collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 		http.Handle(cfg.Prometheus.Endpoint, promhttp.HandlerFor(app.PromRegistry, promhttp.HandlerOpts{}))
 	}
 	if err = app.Start(); err != nil {
